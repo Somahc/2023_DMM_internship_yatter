c3b6db80d8285af2392c99af56bad85240a2a7c8
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-26 15:31:48 +0900

    LoginUiStateのemptyメソッド

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/login/LoginUiState.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/login/LoginUiState.kt
@@ -7,4 +7,16 @@ data class LoginUiState(
     val validPassword: Boolean,
 ) {
     val isEnableLogin: Boolean = validUsername && validPassword
+
+    companion object {
+        fun empty(): LoginUiState = LoginUiState(
+            loginBindingModel = LoginBindingModel(
+                username = "",
+                password = ""
+            ),
+            validUsername = false,
+            validPassword = false,
+            isLoading = false,
+        )
+    }
 }
