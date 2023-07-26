34124541d7e46b28749b4339c0250108fdc5110f
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-26 12:34:23 +0900

    PublicTimelinePage移動, ActivityとPageの繋ぎこみ

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineActivity.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineActivity.kt
@@ -3,7 +3,10 @@ package com.dmm.bootcamp.yatter2023.ui.timeline
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Surface
+import com.dmm.bootcamp.yatter2023.ui.theme.Yatter2023Theme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PublicTimelineActivity : AppCompatActivity() {
@@ -18,6 +21,14 @@ class PublicTimelineActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            Yatter2023Theme {
+                Surface {
+                    PublicTimelinePage(viewModel = viewModel)
+                }
+            }
+        }
     }
 
     override fun onResume() {
--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelinePage.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelinePage.kt
@@ -1,4 +1,4 @@
-package com.dmm.bootcamp.yatter2023.ui.timeline.bindingmodel
+package com.dmm.bootcamp.yatter2023.ui.timeline
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -6,6 +6,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dmm.bootcamp.yatter2023.ui.timeline.PublicTimelineTemplate
 import com.dmm.bootcamp.yatter2023.ui.timeline.PublicTimelineViewModel
 
+// viewModelから状態を取り出し、Templateコンポーザブルに渡す
 @Composable
 fun PublicTimelinePage(viewModel: PublicTimelineViewModel) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
