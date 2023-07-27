4d7524cfa0e034fccc82a241c09103d894b3b0af
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-27 16:26:04 +0900

    PublicTimelineTemplateにFBA表示

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/timeline/PublicTimelineTemplate.kt
@@ -14,9 +14,13 @@ import com.dmm.bootcamp.yatter2023.ui.timeline.bindingmodel.StatusBindingModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -41,6 +45,14 @@ fun PublicTimelineTemplate(
                     Text(text = "タイムライン")
                 },
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onClickPost) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "post"
+                )
+            }
         }
     ) {
         Box(
