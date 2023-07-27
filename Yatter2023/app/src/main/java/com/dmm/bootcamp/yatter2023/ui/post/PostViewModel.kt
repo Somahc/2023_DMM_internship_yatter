191194cce1803e9f3ffe47a79a820372a9f6958b
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-27 14:48:29 +0900

    onCreateメソッド実装

---
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/post/PostViewModel.kt
@@ -0,0 +1,45 @@
+package com.dmm.bootcamp.yatter2023.ui.post
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dmm.bootcamp.yatter2023.domain.service.GetMeService
+import com.dmm.bootcamp.yatter2023.usecase.post.PostStatusUseCase
+import com.dmm.bootcamp.yatter2023.util.SingleLiveEvent
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class PostViewModel(
+    private val postStatusUseCase: PostStatusUseCase,
+    private val getMeService: GetMeService,
+) : ViewModel() {
+    private val _uiState: MutableStateFlow<PostUiState> =
+        MutableStateFlow(PostUiState.empty())
+    val uiState: StateFlow<PostUiState> = _uiState
+
+    private val _goBack: SingleLiveEvent<Unit> = SingleLiveEvent()
+    val goBack: LiveData<Unit> = _goBack
+
+    fun onCreate() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+            val me =getMeService.execute() // getMeServiceでログイン済みのユーザを取得し、アバター画像の情報のみUiStateに更新
+
+            val snapshotBindingModel = uiState.value.bindingModel
+            _uiState.update {
+                it.copy(
+                    bindingModel = snapshotBindingModel.copy(avatarUrl = me?.avatar.toString()),
+                    isLoading = false,
+                )
+            }
+        }
+    }
+
+    fun onChangedStatusText(statusText: String) {}
+
+    fun onClickPost() {}
+
+    fun onClickNavIcon() {}
+}
